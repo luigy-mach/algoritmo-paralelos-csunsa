37e6f7975087160e0750aa8e1f47b0565b108612
luigy <luigy.mach.arc@gmail.com>
2017-06-12 03:49:58 -0500

    update

--- openmp/ch_5/monte_carlo.c
+++ openmp/ch_5/monte_carlo.c
@@ -2,43 +2,45 @@
 #include <stdlib.h>
 #include <omp.h>
 
-double num_random();
-
-int main(int argc, char* argv[]) {
-
-   long int number_tosses, number_in_circle;
-   int thread_count, i;
-   double x, y, distance;
-
-   //thread_count = strtol(argv[1], NULL, 10);
-   //number_tosses = strtoll(argv[2], NULL, 10);
-   thread_count = 8;
-   number_tosses = 19999999;
-
-   number_in_circle =0;
-   srandom(0);
-
-   #pragma omp parallel for num_threads(thread_count) \
-      reduction(+: number_in_circle) private(x, y, distance)
-   for (i = 0; i < number_tosses; i++) {
-      x = num_random(); 
-      y = num_random();
-      distance = x*x + y*y;
-
-      if (distance <= 1) {
-         number_in_circle += 1;
-      }
-   }
-
-   double pi = 4*number_in_circle/((double) number_tosses);
-   printf("Estimation de pi = %.14f\n", pi);
-   return 0;
-}
-
-double num_random() {
+double my_random() {
    double numero = (double) random() / (double) RAND_MAX;
    if((double) random() / (double) RAND_MAX < 0.5) {
       numero *= -1;
    }
    return numero;
 }
+
+
+int main(int argc, char* argv[]) {
+
+   long int number_tosses;
+   long int number_in_circle;
+
+   int thread_count, i;
+   double x, y, distance;
+
+   thread_count = 24;
+   number_tosses = 1000000000; // 10000000 -> 8 threads
+
+   number_in_circle =0;
+
+   srandom(time(NULL));
+   #pragma omp parallel for num_threads(thread_count) \
+      reduction(+: number_in_circle) private(x, y, distance)
+      for (i = 0; i < number_tosses; i++) {
+         x = my_random(); 
+         y = my_random();
+         distance = x*x + y*y;
+
+         if (distance <= 1) {
+            number_in_circle += 1;
+         }
+      }
+
+   double pi = 4 * number_in_circle/((double) number_tosses);
+
+   printf("Estimation de pi = %.14f\n", pi);
+
+   return 0;
+}
+
